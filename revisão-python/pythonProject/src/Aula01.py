8e4d294590fa0d92c3cb19b22eb8d5548443bbe4
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-11-05 08:30:04 -0300

    commit inicial

---
+++ revisão-python/pythonProject/src/Aula01.py
@@ -0,0 +1,65 @@
+"""
+print("Teste")
+
+num1 = 10
+num2 = 20
+
+#Operações
+
+#soma
+print(num1 + num2)
+resultado = num1 + num2
+print(resultado)
+
+#subtração
+print(num1 - num2)
+
+#multiplicação
+print(num1 * num2)
+
+#divisão
+print(num1 / num2)
+
+#módulo
+print(num1 % 2)
+"""
+
+#Saída de dados
+
+"""
+nome = "Hospitalina"
+idade = 50
+peso = 55.3
+
+print("Dados da pessoa:")
+#Forma antiga
+print("%s tem %d anos e pesa %.1f kg" % (nome, idade, peso))
+print("----------------------------------------")
+#Forma nova
+print("{} tem {} anos e pesa {} kg.".format(nome, idade, peso))
+print("----------------------------------------")
+#Forma nova da nova
+print(f"{nome} tem {idade} anos e pesa {peso} kg.")
+print("----------------------------------------")
+"""
+
+"""
+#Entrada de dados
+nome = input("Digite seu nome: ")
+idade = input("Digite sua idade: ")
+peso = input("Digite seu peso: ")
+
+print("----------------------------------------")
+print("{} tem {} anos e pesa {} kg.".format(nome, idade, peso))
+print("----------------------------------------")
+"""
+
+#Estruturas condicionais
+
+numero = int(input("Digite um numero: "))
+if numero > 0:
+    print("Este número é positivo!")
+elif numero < 0:
+    print("Este número é negativo!")
+else:
+    print("O número é 0!")
---
+++ revisão-python/pythonProject/src/Aula02.py
@@ -0,0 +1,15 @@
+#If, elif e else
+
+"""
+nota = float (input("Digite a nota: "))
+if nota >= 7:
+    print("Aprovado!")
+elif nota >= 5:
+    print("Recuperação!")
+else:
+    print("Reprovado!")
+"""
+
+letra = input("Digite uma letra: ").lower()
+if letra in "aeiou":
+    print("É uma vogal")
